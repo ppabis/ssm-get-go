ff21799dc7a9664dc662c42f5e4348f6b295c067
ppabis <piotr@pabis.eu>
2023-08-23 11:43:09 +0200

    actually overwrite default conf

--- 40-nginx-ssm.sh
+++ 40-nginx-ssm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
-ssm-get /nginx/config /etc/nginx/conf.d/fromssm.conf
+ssm-get /nginx/config /etc/nginx/conf.d/default.conf
 # For testing purposes, print the file to logs
-cat /etc/nginx/conf.d/fromssm.conf
+cat /etc/nginx/conf.d/default.conf
