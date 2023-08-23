29e2e674933937b61d91b0ee7af6ad1ce79faee0
ppabis <piotr@pabis.eu>
2023-08-23 10:26:01 +0200

    creating config and arguments to named variables

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
 )
 
 func main() {
@@ -10,4 +14,17 @@ func main() {
 		fmt.Printf("Usage: %s <ParameterPath> <OutputFile>\n", os.Args[0])
 		os.Exit(0)
 	}
+
+	region := "eu-central-1"
+	if os.Getenv("AWS_REGION") != "" {
+		region = os.Getenv("AWS_REGION")
+	}
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
+	if err != nil {
+		fmt.Printf("[ERROR] unable to load SDK config, %v\n", err)
+	}
+
+	parameterName := os.Args[1]
+	fileName := os.Args[2]
 }
